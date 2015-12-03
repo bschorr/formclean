6393a31fd8c6570a46065a2e9db5758f262facba
Bernardo Schorr <bernardo.schorr@gmail.com>
2015-12-03 00:34:56 -0500

    updating for heroku

--- routes/scrape.js
+++ routes/scrape.js
@@ -2,6 +2,10 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var beautify_html = require('js-beautify').html;
+var path = require('path');
+var mime = require('mime');
+
 
 var router = express.Router();
 
@@ -12,10 +16,33 @@ router.post('/', function(req, res, next) {
 
   request(url, function(error, response, html){
     if(!error){
-      res.send("'"+ html + "'");
+
+      //load html to cheerio
+      var $ = cheerio.load(html);
+
+      //extract html form tag and beautify it
+      var form = $.html('form');
+      form = (beautify_html(form, { indent_size: 2 }));
+
+      //write file to server
+      fs.writeFile('form.html', form, function(err){
+        console.log('File successfully written! - Check your project directory for the form.html file');
+      })
+
+      //auto download magic
+      var file = __dirname + '/../form.html';
+
+      var filename = path.basename(file);
+      var mimetype = mime.lookup(file);
+
+      res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+      res.setHeader('Content-type', mimetype);
+
+      var filestream = fs.createReadStream(file);
+      filestream.pipe(res);
+
     }
   })
-
 });
 
 module.exports = router;
